67ee742e271d425d8c96e40b8d3623e4e11a0934
ILaxaiI <58485516+ILaxaiI@users.noreply.github.com>
2019-12-04 15:37:19 +0100

    Add files via upload

---
+++ state.lua
@@ -0,0 +1,50 @@
+--uncomment the things if you also implemented my gui lib
+--local gui = require("gui")
+
+local state = {}
+local States = {}
+
+do 
+local functions = {
+        "draw",
+        "update",
+        "mousepressed",
+        "mousereleased",
+        "mousemooved",
+        "keypressed",
+      }
+
+  -- auto generate functions for the scenes because im lazy
+  for _,v in ipairs(functions) do
+    if not state[v] then
+    
+      state[v] = function(...)
+        if state.active and States[state.active] and States[state.active][v] then 
+          States[state.active][v](...)
+        end
+
+      end
+    
+    end  
+  end
+end
+
+
+--alternatively manually require the scene to the scenes table
+function state.load(filePath,name)
+  States[name or filePath] = require(filePath)
+end
+
+function state.set(name)
+  if States[name] then
+    state.active = name
+  end
+end
+
+function state.getStates()
+  return States
+end
+
+
+
+return state
