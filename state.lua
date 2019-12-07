c857b7178f6669106202e6745ecdb105d24e9c8d
ILaxaiI <58485516+ILaxaiI@users.noreply.github.com>
2019-12-07 14:03:55 +0100

    Update state.lua

--- state.lua
+++ state.lua
@@ -1,42 +1,29 @@
---uncomment the things if you also implemented my gui lib
---local gui = require("gui")
-
-local state = {}
+local meta={}
+local state = setmetatable({},meta)
 local States = {}
 
-do 
-local functions = {
-        "draw",
-        "update",
-        "mousepressed",
-        "mousereleased",
-        "mousemooved",
-        "keypressed",
-      }
-
-  -- auto generate functions for the scenes because im lazy
-  for _,v in ipairs(functions) do
-    if not state[v] then
-    
-      state[v] = function(...)
-        if state.active and States[state.active] and States[state.active][v] then 
-          States[state.active][v](...)
-        end
-
-      end
-    
-    end  
+local function createCallback(v)
+  return function(...)
+    if States[state.active] and States[state.active][v] then 
+      States[state.active][v](...)
+    end
   end
 end
 
+function meta:__index(key)
+  self[key] = createCallback(key)
+  return self[key]
+end
 
---alternatively manually require the scene to the scenes table
+--alternatively manually require the scene to the States table
 function state.load(filePath,name)
-  States[name or filePath] = require(filePath)
+  local err
+  err,States[name or filePath] = pcall(require,filePath)
+  return err
 end
 
 function state.set(name,...)
-  if States[name] then
+  if States[name] or state.load(name) then
     if States[name].init then States[name].init(...) end
     state.active = name
   end
@@ -45,7 +32,5 @@ end
 function state.getStates()
   return States
 end
-
-
 
 return state
